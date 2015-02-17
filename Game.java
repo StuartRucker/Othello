84f59a6952b3d8d10122e1541a3f2259cd0c9f72
StuartRucker <Stuart.a.rucker@gmail.com>
2015-02-16 21:12:01 -0500

    Merge GUI and Backend
    few functions added, with no body

--- Game.java
+++ Game.java
@@ -1,34 +1,39 @@
 class Game {
-	private Board b;
-	private Engine e;
-	//private AI ai;
+    private Board b;
+    private Engine e;
+    //private AI ai;
 
-	public Game() {
-		this(8,8);
-	}
+    public Game() {
+        this(8,8);
+    }
 
-	public Game(int w, int h) {
-		b = new Board(w,h);
-		e = new Engine();
-		//ai = new AI();
-	}
+    public Game(int w, int h) {
+        b = new Board(w,h);
+        e = new Engine();
+        //ai = new AI();
+    }
 
-	public byte get(int i, int j) {
-		return b.get(i,j);
-	}
+    public boolean play(int i, int j) {
+        b.place(i,j);
+        /*if (e.capture(b,i,j)) {
+            return true;
+        } else {
+            System.out.println("Illegal Move");
+            b.revert(i,j);
+            return false;
+        }*/
+        return true;
+    }
 
-	public boolean play(int i, int j) {
-		b.place(i,j);
-		if (e.capture(b,i,j)) {
-			return true;
-		} else {
-			System.out.println("Illegal Move");
-			b.revert(i,j);
-			return false;
-		}
-	}
-
-	public void print() {
-		System.out.println(b.toString());
-	}
+    public void print() {
+        System.out.println(b.toString());
+    }
+    public Board getBoard(){
+        return b;
+    }
+    public boolean isValidMove(int x, int y){
+        //to do
+        return true;
+    }
+        
 }
