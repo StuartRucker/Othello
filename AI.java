38515f65e50fa5ce42c04bb13ff4b0c6bd33fc61
Vin-Deisel <vkurup@exeter.edu>
2015-02-24 11:26:04 -0500

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,6 +1,14 @@
+/*
+*
+* @Author: Vinayak Kurup, Stuart Rucker, Oscar Suen
+* @Version: February 24, 2015
+*
+* AI Class-- runs the AI for the game
+*/
 
 public class AI {
 	GameState current;
+	//Initializes Board
 	public AI(Board init, int depth) {
 		current = new GameState(init, (byte)1, (byte)(-1), (byte)(-1));
 		generateDepth(0, depth, current);
@@ -19,6 +27,7 @@ public class AI {
 		}
 	}
 
+	//Computes the value of the board by iterating through GameState children
 	public double computeValue(GameState root) {
 		if (root.getChildren().isEmpty()) {
 			root.addValue(root.getScore());
@@ -34,13 +43,16 @@ public class AI {
 			return root.getValue();
 		}
 	}
-
+	
+	//Returns the board that has the best move (relying on computeValue()
 	public Board bestMove(GameState root) {
-		double m = Double.MAX_VALUE;
+		double min = Double.MAX_VALUE;
 		Board b = null;
-		for (GameState c : root.getChildren()) {
-			if (m > c.getValue()) { //Stuart wtf > was correct
-				m = c.getValue();
+		for (GameState c : root.getChildren()) 
+		{
+			if (min > c.getValue()) 
+			{ //Stuart wtf > was correct
+				min = c.getValue();
 				b = c.getBoard();
 				current = c;
 			}
@@ -48,13 +60,18 @@ public class AI {
 		return b;
 	}
 
-	public GameState getCurrent() {
+	//Current gamestate
+	public GameState getCurrent() 
+	{
 		return current;
 	}
 
-	public void update(Board b, byte x, byte y) {
-		for (GameState s : current.getChildren()) {
-			if (s.getX() == x && s.getY() == y) { //Why no worky-worky???
+	public void update(Board b, byte x, byte y) 
+	{
+		for (GameState s : current.getChildren()) 
+		{
+			if (s.getX() == x && s.getY() == y) 
+			{ //Why no worky-worky... HMM let's figure it out
 				current = s;
 				return;
 			}
